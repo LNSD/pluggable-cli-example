d4bbfbd0a764acd2f4f09540186d7050766aa2c5
Lorenzo Delgado <lorenzo.delgado@lnsd.es>
2019-12-23 20:22:51 +0100

    Added extra command

--- pluggable-cli-plugin/pcli_plugin/cli.py
+++ pluggable-cli-plugin/pcli_plugin/cli.py
@@ -3,12 +3,43 @@
 import click
 
 import pcli.plugin
+import pcli_plugin
 
 
 @click.group()
 def workflow():
     """Command group coming from the pluggable-cli-plugin."""
-    click.echo("Workflow command")
+    pass
+
+
+@click.command()
+def version():
+    """Version example command."""
+    click.echo("PCLI Example plugin v{}".format(pcli_plugin.__version__))
+
+
+@click.command()
+def clone():
+    """Clone example command."""
+    click.echo("Clone command")
+
+
+@click.command()
+def fetch():
+    """Clone example command."""
+    click.echo("Fetch command")
+
+
+@click.command()
+def branch():
+    """Branch example command."""
+    click.echo("Branch command")
+
+
+workflow.add_command(version)
+workflow.add_command(clone)
+workflow.add_command(fetch)
+workflow.add_command(branch)
 
 
 @pcli.plugin.hookimpl
@@ -17,5 +48,4 @@ def pcli_get_commands():
 
     :return: List of commands
     """
-    return [v for v in globals().values()
-            if isinstance(v, click.core.BaseCommand)]
+    return [workflow]
